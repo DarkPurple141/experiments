905935fd7a47f5be615de1ba88126a49073b27ec
Alex Hinds <alex.hinds141@gmail.com>
2018-04-22 18:01:07 +1000

    now with actual virtual dom

--- virtualdom/app.js
+++ virtualdom/app.js
@@ -1,4 +1,61 @@
 
+// generate a vNode
+function generateVNode(node, children) {
+   if (typeof node === 'string')
+      return node
+   if (node.children)
+      children = children.map(child => generateVNode(child.node, child.children))
+   return { node: node.tag.toUpperCase(), children: children || []}
+}
+
+// create a real element from a vNode
+function createElement(vnode) {
+  if (typeof vnode === 'string') {
+    return document.createTextNode(vnode)
+  }
+
+  const $el = document.createElement(vnode.node.toLowerCase())
+  for (let child of vnode.children) {
+      $el.appendChild(createElement(child))
+  }
+  return $el
+}
+
+/**
+    @param Node       $node --- A real DOM node
+    @param Object     vnode --- vDom representation
+*/
+function equals($node, vnode) {
+   if (typeof vnode === 'string') {
+      return $node.nodeValue == vnode
+   }
+
+   return vnode.node == $node.nodeName
+}
+
+// Update the DOM
+function updateDOM($parent, node, vnode) {
+   // node doesn't exit
+   if (!node) {
+      $parent.appendChild(createElement(vnode))
+   // vnode doesn't exist (ie real node needs to be deleted)
+   } else if (!vnode) {
+      $parent.removeChild(node)
+   // else nodes aren't equal
+   } else if (!equals(node, vnode)) {
+      $parent.replaceChild(createElement(vnode), node)
+   // else nodes are equal
+   } else {
+
+      // update children if needed
+      for (let i in vnode.children) {
+         updateDOM(node, node.childNodes[i], vnode.children[i])
+      }
+   }
+
+}
+
+
 function Model(fetch) {
    this.post = 0
    this.getPost = function() {
@@ -8,6 +65,13 @@ function Model(fetch) {
 
    this.getViewModel = function() {
       return this.getPost(this.post++)
+         .then(model => {
+            const virtualDOM = generateVNode({ tag: 'section' }, [])
+            virtualDOM.children.push(generateVNode({ tag: 'h3' }, [ model.title ]))
+            virtualDOM.children.push(generateVNode({ tag: 'article' },
+               model.body.split('\n').map(line => generateVNode({ tag: 'P' }, [ line ]))))
+            return [ virtualDOM ]
+         })
    }
 }
 
@@ -15,12 +79,10 @@ function Model(fetch) {
 function View(root) {
    this.root = root
 
-   this.render = function(article) {
-      this.root.innerHTML =
-      `<section class="article">
-         <h3 class="article-header">${article.title}</h3>
-         <article class="article-content">${article.content}</article>
-      </section>`
+   this.render = function(vDOM) {
+      for (let child in vDOM) {
+         update(this.root, this.root.childNodes[child], vDOM[child])
+      }
    }
 }
 
@@ -33,39 +95,30 @@ function nextButton() {
    return button
 }
 
-const Controller = {
+const App = {
 
    view : null,
    model : null,
-   button: null,
 
-   init: function({ view, model, button }) {
+   init (view, model, button) {
       this.view   = view
       this.model  = model
       this.button = button
-      this.view.render({title: "Nothing yet.", content: "Click the button"})
    },
 
-   triggerUpdate: function() {
+   triggerUpdate() {
       this.model.getViewModel()
-         .then(data => {
-            return {
-               content: data.body,
-               title  : data.title,
-               author: "Alex"
-            }
-         })
          .then(this.view.render)
    }
 }
 
 function init() {
-   const root = document.getElementById('root')
+   const root       = document.getElementById('root')
    const view       = new View(root)
    const model      = new Model(window.fetch)
    const button     = nextButton()
-   Controller.init({ view, model, button })
-   button.addEventListener('click', Controller.triggerUpdate.bind(Controller))
+   App.init( view, model)
+   button.addEventListener('click', App.triggerUpdate.bind(App))
 }
 
 document.addEventListener('DOMContentLoaded', init)
